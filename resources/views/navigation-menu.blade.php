def96c4cd4c00ada89867c5183b6de589f2b153e
Eric <eric@Emmas-MacBook-Air.local>
2023-11-14 14:32:41 -0800

    add lists to mobile nav

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -173,6 +173,11 @@
                 {{ __('Add Recipe') }}
             </x-responsive-nav-link>
         </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link href="{{ route('lists.show') }}" :active="request()->routeIs('lists.show')">
+                {{ __('Lists') }}
+            </x-responsive-nav-link>
+        </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
