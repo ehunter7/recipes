38c9e87a13f27c1796ba398ee51ba7f02775e521
Eric <eric@Emmas-MacBook-Air.local>
2023-11-20 21:23:09 -0800

    revert photo size

--- app/Http/Livewire/CreateRecipe.php
+++ app/Http/Livewire/CreateRecipe.php
@@ -127,7 +127,7 @@ class CreateRecipe extends Component
     {
         $this->validate([
             // 1MB Max
-            'photo' => 'image|max:4032',
+            'photo' => 'image|max:1024',
             'name' => 'required|min:2|max:36',
             'category' => 'required|min:2|max:36',
             'yield' => 'required|integer',
